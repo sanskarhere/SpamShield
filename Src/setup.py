013bc8e7ec456ee38c9f373913ef4a3ba7c9d353
sanskarhere <sanskargupta693@gmail.com>
2026-03-20 18:59:22 +0530

    Setup

---
+++ Src/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='SpamShield',
+    version='1.0.0',
+    description='A powerful anti-spam Model For Sms',
+    author='Sanskar Gupta',
+    packages=find_packages(),
+    libraries=['Numpy,pandas,sklearn,joblib']
+)
